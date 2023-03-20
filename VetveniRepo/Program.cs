aa254c586a24b9acc9dcf4182fc4e99024e9ab88
Dalibor Klíma <dalibor.klima@educhem.cz>
2023-03-20 10:39:39 +0100

    hodně věcí jsem přidal

--- VetveniRepo/Program.cs
+++ VetveniRepo/Program.cs
@@ -4,6 +4,27 @@
     "aj",
     "čj", "d", "m", "ikt", "os", "f", "tv", "opř", "zel" };
 
+string[] predmetyDruhak = {"čj",
+    "aj",
+    "d",
+    "zsv",
+    "m",
+    "tv", "f", "hw", "opř", "ikt", "os", "zw", "as", "..." };
+
+string[] predmetyTretak = {"čj",
+    "aj",
+    "zsv",
+    "f",
+    "m", "tv", "e", "pg", "hw", "..."};
+
+string[] predmetyCtvrtak = {"čj", 
+    "aj",
+    "m",
+    "tv",
+    "vr",
+    "dp",
+    "..."};
+
 Console.WriteLine("Vyber ročník 1 - 4");
 string vyberUzivatele = Console.ReadLine();
 
@@ -13,8 +34,30 @@ switch(vyberUzivatele)
         foreach(var item in predmetyPrvak)
     {
         Console.WriteLine(item);
-     }
+    }
     break;
+
+    case "2":
+        foreach(var item in predmetyDruhak)
+        {
+            Console.WriteLine(item);
+        }
+        break;
+    
+    case "3":
+        foreach(var item in predmetyTretak)
+        {
+            Console.WriteLine(item);
+        }
+        break;
+
+    case "4":
+            foreach(var item in predmetyCtvrtak)
+        {
+            Console.WriteLine(item);
+        }
+        break;
+
     default: 
         Console.WriteLine("Coming soon");
     break;
