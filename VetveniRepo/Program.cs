0a02389b94f1b8412861fcc792f51ac4525486ac
Dalibor Klíma <dalibor.klima@educhem.cz>
2023-03-20 11:02:12 +0100

    zaměření

--- VetveniRepo/Program.cs
+++ VetveniRepo/Program.cs
@@ -4,20 +4,23 @@
     "aj",
     "čj", "d", "m", "ikt", "os", "f", "tv", "opř", "zel" };
 
-string[] predmetyDruhak = {"čj",
+string[] predmetyDruhak = {
+    "čj",
     "aj",
     "d",
     "zsv",
     "m",
     "tv", "f", "hw", "opř", "ikt", "os", "zw", "as", "..." };
 
-string[] predmetyTretak = {"čj",
+string[] predmetyTretak = {
+    "čj",
     "aj",
     "zsv",
     "f",
     "m", "tv", "e", "pg", "hw", "..."};
 
-string[] predmetyCtvrtak = {"čj", 
+string[] predmetyCtvrtak = {
+    "čj", 
     "aj",
     "m",
     "tv",
@@ -27,7 +30,7 @@ string[] predmetyCtvrtak = {"čj",
 
 Console.WriteLine("Vyber ročník 1 - 4");
 string vyberUzivatele = Console.ReadLine();
-
+string zamereni = Console.ReadLine();
 switch(vyberUzivatele)
 {
     case "1":
@@ -45,14 +48,44 @@ switch(vyberUzivatele)
         break;
     
     case "3":
-        foreach(var item in predmetyTretak)
+        Console.WriteLine("napiš zaměření");
+        if (zamereni == "HWPOS")
+        {
+            Console.WriteLine("shw");
+            Console.WriteLine("sps");
+            Console.WriteLine("iv");
+            Console.WriteLine("ass");
+        }
+        else
+        {
+            Console.WriteLine("opg");
+            Console.WriteLine("mv");
+            Console.WriteLine("gs");
+            Console.WriteLine("wapp");
+        }
+        foreach (var item in predmetyTretak)
         {
             Console.WriteLine(item);
         }
         break;
 
     case "4":
-            foreach(var item in predmetyCtvrtak)
+        Console.WriteLine("napiš zaměření");
+        if(zamereni == "HWPOS")
+        {
+            Console.WriteLine("shw");
+            Console.WriteLine("sps");
+            Console.WriteLine("iv");
+            Console.WriteLine("asy");
+        }
+        else
+        {
+            Console.WriteLine("opg");
+            Console.WriteLine("mv");
+            Console.WriteLine("gs");
+            Console.WriteLine("wapp");
+        }
+        foreach (var item in predmetyCtvrtak)
         {
             Console.WriteLine(item);
         }
