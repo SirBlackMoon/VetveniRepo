c219abd054084e03f8b3fee6901765526aa02525
Dalibor Klíma <dalibor.klima@educhem.cz>
2023-03-20 10:08:06 +0100

    Přidejte soubory projektu.

---
+++ VetveniRepo/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] predmetyPrvak = { 
+    "alp",
+    "hw",
+    "aj",
+    "čj", "d", "m", "ikt", "os", "f", "tv", "opř", "zel" };
+
+Console.WriteLine("Vyber ročník 1 - 4");
+string vyberUzivatele = Console.ReadLine();
+
+switch(vyberUzivatele)
+{
+    case "1":
+        foreach(var item in predmetyPrvak)
+    {
+        Console.WriteLine(item);
+     }
+    break;
+    default: 
+        Console.WriteLine("Coming soon");
+    break;
+}
